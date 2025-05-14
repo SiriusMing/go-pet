1ac5a956b87d3116374656680f7a1d6cb883da87
root <root@Richard.localdomain>
2025-05-14 18:36:31 +0800

    清理所有main 分支残留

--- internal/config/config.go
+++
@@ -1,6 +0,0 @@
-// internal/config/config.go
-type Config struct {
-    DBHost     string `mapstructure:"DB_HOST"`
-    DBPort     int    `mapstructure:"DB_PORT"`
-    ServerPort int    `mapstructure:"SERVER_PORT"`
-}
